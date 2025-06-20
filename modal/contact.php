a3e807b75b7f9f3b01d01c438056769f84e4ca75
JiyadAhammad <jiyadahammad99@gmail.com>
2025-06-20 22:54:10 +0530

    cv uplaoded in pdf type , contact links added in bottom section

--- modal/contact.php
+++ modal/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Put contacting email here
-$php_main_email = "yourmail@gmail.com";
+$php_main_email = "jiyadahammad74@gmail.com";
 
 //Fetching Values from URL
 $php_name = $_POST['ajax_name'];
